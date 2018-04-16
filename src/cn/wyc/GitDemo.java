e87ac155ef0893052904581ace2b3c525fb3fbb1
BG343093 <18758038180@163.com>
2018-04-16 20:23:02 +0800

    IDEA+Git+GitHub测试

---
+++ src/cn/wyc/GitDemo.java
@@ -0,0 +1,10 @@
+package cn.wyc;
+
+public class GitDemo {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        System.out.println("hello2");
+        System.out.println("123");
+        System.out.println("123");
+    }
+}
